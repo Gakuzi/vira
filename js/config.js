a6ff14e6bce44282d755f4139ee5820a3b588f52
Евгений Климов <eklimov84@gmail.com>
2025-09-30 09:20:41 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// js/config.js
+export const SUPABASE_URL = 'https://effpidrlccjmnxmbtkiz.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmZnBpZHJsY2NqbW54bWJ0a2l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNDQ5NDUsImV4cCI6MjA3NDcyMDk0NX0.SwJumspCD2x1Fsul4yw2LBL-OSj-4giXeBl99D1fFLg';
+export const AUDITOR_EMAIL = 'eklimov84@gmail.com';
+export const REDIRECT_URL = 'https://gakuzi.github.io/vira/auditor.html';
