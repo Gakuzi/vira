16e389cbe6e807952c6f0950102b0c9c2f14f2ec
Евгений Климов <eklimov84@gmail.com>
2025-09-30 09:43:01 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,3 +3,7 @@ export const SUPABASE_URL = 'https://effpidrlccjmnxmbtkiz.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmZnBpZHJsY2NqbW54bWJ0a2l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNDQ5NDUsImV4cCI6MjA3NDcyMDk0NX0.SwJumspCD2x1Fsul4yw2LBL-OSj-4giXeBl99D1fFLg';
 export const AUDITOR_EMAIL = 'eklimov84@gmail.com';
 export const REDIRECT_URL = 'https://gakuzi.github.io/vira/auditor.html';
+
+// Для Telegram-уведомлений (настройте бота и вставьте сюда)
+export const TELEGRAM_BOT_TOKEN = '7930584230:AAFfR2XnKjTrLsP-LrWICLlj83c2nNG41-8';
+export const TELEGRAM_CHAT_ID = '55570186';
